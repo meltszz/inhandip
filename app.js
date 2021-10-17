11dea4d040f7bb11d48b3eae5e5957e2199e10b7
Leandro Jesus <88406287+xarcodex@users.noreply.github.com>
2021-10-17 23:20:06 +0000

    Added mainIPC to app.js plus listeners

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain} = require('electron');
 const path = require('path');
+const publicIP = require('public-ip');
+let win;
 
 function createWindow() {
-    const win = new BrowserWindow({
+    win = new BrowserWindow({
         width: 800,
         height: 600,
         webPreferences: {
@@ -18,12 +20,22 @@ function createWindow() {
 
 app.on('window-all-closed', function () {
     if (process.platform !== 'darwin') app.quit()
-  })
+});
 
-  app.whenReady().then(() => {
+app.whenReady().then(() => {
     createWindow()
-  
+
     app.on('activate', function () {
       if (BrowserWindow.getAllWindows().length === 0) createWindow()
     })
-  })
+});
+
+async function getIPV4() {
+    return publicIP.v4();
+}
+
+ipcMain.on("send-ipv4", () => {
+    getIPV4().then((ipv4) => {
+        win.webContents.send("receive-ipv4", ipv4);
+    }).catch(err => console.log(err));
+});
