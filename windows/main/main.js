7cd1ee43c671f56e225e37559df069a1fe791d83
Leandro Jesus <88406287+xarcodex@users.noreply.github.com>
2021-10-18 21:01:10 +0000

    Added copy ip to clipboard using 'copy ip button'

--- windows/main/main.js
+++ windows/main/main.js
@@ -1,2 +1,8 @@
 window.api.send("send-ipv4");
 window.api.receive("receive-ipv4", (e, ipv4) => $("#lbl-ip").text(ipv4));
+
+$('#btn-copy-ip').on('click', () => {
+    const ipv4 = $('#lbl-ip').text();
+    console.log(ipv4)
+    navigator.clipboard.writeText(ipv4);
+});
