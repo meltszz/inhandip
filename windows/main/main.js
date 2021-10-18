834d1771b8a5f6ea10c2634bbefce322e0e0713e
Leandro Jesus <88406287+xarcodex@users.noreply.github.com>
2021-10-18 21:16:58 +0000

    Added check ip using 'check ip button'

--- windows/main/main.js
+++ windows/main/main.js
@@ -1,8 +1,13 @@
-window.api.send("send-ipv4");
-window.api.receive("receive-ipv4", (e, ipv4) => $("#lbl-ip").text(ipv4));
+window.api.send('send-ipv4');
+window.api.receive('receive-ipv4', (e, ipv4) => {
+    $("#lbl-ip").text(ipv4);
+});
+
+$("#btn-check-ip").on('click', () => {
+    window.api.send('send-ipv4');
+});
 
 $('#btn-copy-ip').on('click', () => {
     const ipv4 = $('#lbl-ip').text();
-    console.log(ipv4)
     navigator.clipboard.writeText(ipv4);
 });
